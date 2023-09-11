80c99a1e4b5f49a9d94a23336b1df0ae009a8e4c
William Kang <wkang2020@gmail.com>
2023-09-10 22:30:26 -0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :books do
-    member do 
-      get 'delete'
-    end
-  end
+  resources :books 
+  # do
+  #   member do 
+  #     get 'delete'
+  #   end
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
