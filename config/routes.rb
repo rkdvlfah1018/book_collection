a586b615d810d0e13bd8fdc12704496d6e657e90
William Kang <wkang2020@gmail.com>
2023-09-10 23:12:36 -0500

    remove procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :books 
-  # do
-  #   member do 
-  #     get 'delete'
-  #   end
-  # end
+  resources :books do
+    member do 
+      get 'delete'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
